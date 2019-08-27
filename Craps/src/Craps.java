aa589e1d5a0cedae88cc870fd69b58b3648225c6
michael.nguyen <michael.nguyen@MU-C1-18.mullen.pvt.k12.co.us>
2019-08-27 11:50:19 -0600

    craps working

--- Craps/src/Craps.java
+++ Craps/src/Craps.java
@@ -24,23 +24,52 @@ public class Craps
 				int diceSum = dice1 + dice2;
 			
 				boolean dice = false;
+				boolean firstRoll = true; 
+				boolean roll2 = false; 
 
 				if (diceSum == 12 || diceSum == 2)
 					{
-						System.out.println("You lose Sucker.");
+						System.out.println(" You rolled a: " + diceSum + "You lose Sucker.");
+						firstRoll = false;
 					}
 				else if(diceSum == 7 || diceSum == 11 )
 					{
-						System.out.println("You automaticlly win, WOW!");
+						System.out.println("You rolled: " + diceSum + " You automaticlly win, WOW!");
+						firstRoll = false;
 					}
 			
 			
-				else 
+				else
 					{
+						System.out.println("your points is " + diceSum + ". Roll again");
+						roll2 = true;
+				
+					
+					}
+				while (roll2)
+					{
+						int randomNumber3 = (int) (Math.random() *6 + 1);
+						int randomNumber4 = (int) (Math.random() *6 + 1);
+						String rollA= userInput.nextLine(); 
+						int diceA = randomNumber3; 
+						int diceB = randomNumber4; 
+						int diceSumA = diceA + diceB;
+						int userPoint = diceSum;
 						
-						int point = diceSum;
-						System.out.println("your points is " + diceSum);
-						
+						if (diceSumA == userPoint)
+							{
+								System.out.println(" You rolled a: " + diceSumA + " You Win. Amazing!");
+								roll2 = false;
+							}
+						if (diceSumA == 7)	
+							{
+								System.out.println("You rolled: " + diceSumA + ". YOU LOSE SUCKER.");
+								roll2 = false;
+							}
+						else 
+							{
+								System.out.println("Your point is " + userPoint + " You rolled: " + diceA + " and " + diceB + " For a total of " + diceSumA + " Please roll again.");
+							}
 					}
 			}
 }
